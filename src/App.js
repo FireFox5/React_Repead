94f210231d55cc08e722c795b118dbb3fcd04d7f
FireFox5 <sultanmuratnaspekov@gmail.com>
2022-06-25 14:09:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+  function AboutUs() {
+      return(
+          <div className='footer' >
+              <h2>Hello my name is Sultanmurat</h2>
+          </div>
+      )
+
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <div>
+            <AboutUs></AboutUs>
+        </div>
     </div>
   );
+
 }
 
 export default App;
